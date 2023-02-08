102e2894ebd5837823359499e89f19a4e34376bd
Sungbae Jeong <almagest0328@gmail.com>
2023-02-08 23:04:07 +0900

    add version in the build script

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # xilo
 An improved `rm` command. The origin of this program name comes from the greek word `ξήλωμα` which means the word `rip` in English.
+
+# How to install
+
+## Use precompiled binary
+Download binary from `release`. All binaries are compiled into `ReleaseSmall` optimize.
+
+## Build from source
+First, you need to 
+```console
+$ zig run install -p <install path> -Doptimize=ReleaseSafe
+```
+where `<install path>` is the path where you want to put compiled executable.
+Then the executable will be saved at `<install path>/bin`.
--- build.zig
+++ build.zig
@@ -4,6 +4,8 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const version = std.builtin.Version{ .major = 0, .minor = 1, .patch = 0 };
+
     const zlap_dep = b.dependency("zlap", .{
         .target = target,
         .optimize = optimize,
@@ -14,6 +16,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
+        .version = version,
     });
     exe.addModule("zlap", zlap_dep.module("zlap"));
     exe.install();
