87920c01d1517d4a313a56ad4b9346bbd263e69c
Sungbae Jeong <almagest0328@gmail.com>
2025-12-31 14:02:52 +0900

    add build script

--- build.zig
+++ build.zig
@@ -33,7 +33,6 @@ pub fn build(b: *Build) !void {
     exe_options.addOption(?[]const u8, "custom_trashbin_path", custom_trashbin_path);
 
     const zlap_module = b.dependency("zlap", .{}).module("zlap");
-
     const win_zig_trans_c = b.addTranslateC(.{
         .root_source_file = b.path("./src/winzig.h"),
         .target = target,
@@ -53,7 +52,7 @@ pub fn build(b: *Build) !void {
             .Debug, .ReleaseSafe => false,
             else => true,
         },
-        .link_libcpp = true,
+        .link_libc = true,
         .imports = &.{
             .{ .name = "zlap", .module = zlap_module },
             .{ .name = "windows", .module = win_zig },
@@ -78,4 +77,61 @@ pub fn build(b: *Build) !void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    // Release
+    const release_step = b.step("release", "Make zigup binaries for release");
+    const targets: []const std.Target.Query = &.{
+        .{ .cpu_arch = .aarch64, .os_tag = .macos },
+        .{ .cpu_arch = .aarch64, .os_tag = .linux },
+        .{ .cpu_arch = .x86_64, .os_tag = .macos },
+        .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+        .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .musl },
+        .{ .cpu_arch = .x86_64, .os_tag = .windows },
+    };
+
+    for (targets) |t| {
+        const cross_target = b.resolveTargetQuery(t);
+
+        const zlap_cross_module = b.dependency("zlap", .{
+            .target = cross_target,
+            .optimize = optimize,
+        }).module("zlap");
+
+        const win_zig_cross_trans_c = b.addTranslateC(.{
+            .root_source_file = b.path("./src/winzig.h"),
+            .target = cross_target,
+            .optimize = optimize,
+        });
+        const win_zig_cross = b.createModule(.{
+            .root_source_file = win_zig_cross_trans_c.getOutput(),
+            .target = cross_target,
+            .optimize = optimize,
+        });
+
+        const release_exe = b.addExecutable(.{
+            .name = "zigup",
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("./src/main.zig"),
+                .target = cross_target,
+                .optimize = .ReleaseSafe,
+                .strip = true,
+                .link_libc = true,
+                .imports = &.{
+                    .{ .name = "zlap", .module = zlap_cross_module },
+                    .{ .name = "windows", .module = win_zig_cross },
+                },
+            }),
+        });
+        release_exe.root_module.addOptions("xilo_build", exe_options);
+
+        const target_output = b.addInstallArtifact(release_exe, .{
+            .dest_dir = .{
+                .override = .{
+                    .custom = try t.zigTriple(b.allocator),
+                },
+            },
+        });
+
+        release_step.dependOn(&target_output.step);
+    }
 }
